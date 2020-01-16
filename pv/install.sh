5f0a1d2e8aa410c2970f1e5e597bb79eb23e4df4
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-01-16 17:19:34 +0800

    save

--- pv/install.sh
+++ pv/install.sh
@@ -2,3 +2,10 @@ kubectl create -f namespace.yaml
 kubectl create -f claim.yaml -n cmengdi
 kubectl get pvc -n cmengdi
 kubectl create -f http-server-pod.yaml -n cmengdi
+
+
+
+
+kubectl delete -f namespace.yaml
+kubectl delete -f claim.yaml -n cmengdi
+kubectl delete -f http-server-pod.yaml -n cmengdi
