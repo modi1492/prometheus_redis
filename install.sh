b186fe4db566304bb9b4645853ed759821cea493
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-01-16 16:34:09 +0800

    save

--- install.sh
+++ install.sh
@@ -34,3 +34,29 @@ kubectl apply -f qa_ibaboss_elk_redis.yaml --kube-context k8s-decc-dev
 kubectl get pods -n qa-ibaboss-elk --kube-context k8s-decc-dev
 kubectl run busybox --image=busybox -n qa-ibaboss-elk --command -- ping baidu.com --kube-context k8s-decc-dev
 
+
+
+
+
+############################################################
+kubectl describe cm redis-conf
+kubectl create -f headless-service.yml
+kubectl get pods -o wide
+kubectl get pv
+kubectl run -it ubuntu --image=ubuntu --restart=Never /bin/bash
+apt-get update
+apt-get install -y vim wget python2.7 python-pip redis-tools dnsutils
+pip install redis-trib==0.5.1
+redis-trib.py replicate \
+  --master-addr `dig +short redis-app-0.redis-service.default.svc.cluster.local`:6379 \
+  --slave-addr `dig +short redis-app-3.redis-service.default.svc.cluster.local`:6379
+
+redis-trib.py replicate \
+  --master-addr `dig +short redis-app-1.redis-service.default.svc.cluster.local`:6379 \
+  --slave-addr `dig +short redis-app-4.redis-service.default.svc.cluster.local`:6379
+
+redis-trib.py replicate \
+  --master-addr `dig +short redis-app-2.redis-service.default.svc.cluster.local`:6379 \
+  --slave-addr `dig +short redis-app-5.redis-service.default.svc.cluster.local`:6379
+kubectl get svc redis-access-service -o wide
+
