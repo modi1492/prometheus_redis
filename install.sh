890031783b2f36b4c05fb7db96331cc604c998be
Mengdi Chen <cmengdi@cmengdi-a01.vmware.com>
2020-01-16 16:08:45 +0800

    save

--- install.sh
+++ install.sh
@@ -19,3 +19,18 @@ helm del --purge nginx-ingress --kube-context k8s-decc-dev
 kubectl get po,svc -n kube-system -o wide
 
 helm ls --all nginx-ingress --kube-context k8s-decc-dev
+
+curl http://10.161.23.126
+curl -I http://10.161.23.126/healthz/
+
+curl -kI  https://k8s.hongda.com/
+
+
+
+###########################################################
+kubectl apply -f namespace.yaml --kube-context k8s-decc-dev
+kubectl create configmap qa-ibaboss-elk-redis-conf --from-file=redis.conf -n qa-ibaboss-elk --kube-context k8s-decc-dev
+kubectl apply -f qa_ibaboss_elk_redis.yaml --kube-context k8s-decc-dev
+kubectl get pods -n qa-ibaboss-elk --kube-context k8s-decc-dev
+kubectl run busybox --image=busybox -n qa-ibaboss-elk --command -- ping baidu.com --kube-context k8s-decc-dev
+
